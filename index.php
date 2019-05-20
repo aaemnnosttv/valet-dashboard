46864a1b4fb05500b1fb1d52c8ad291643de3cc5
Raphael Horber <raphael.horber@gmail.com>
2019-05-20 09:56:30 +0200

    Upgraded tailwind classes to v1.0

--- index.php
+++ index.php
@@ -20,12 +20,12 @@ $tld = isset($valet_config->tld) ? $valet_config->tld : $valet_config->domain;
         <div class="grid">
             <?php foreach ($valet_config->paths as $parked_path) : ?>
                 <div class="leading-normal whitespace-no-wrap">
-                    <code class="font-mono text-grey-dark"><?= str_replace(getenv('HOME'), '~', $parked_path) ?></code>
-                    <ul>
+                    <code class="font-mono text-gray-600"><?= str_replace(getenv('HOME'), '~', $parked_path) ?></code>
+                    <ul class="list-disc pl-4">
                         <?php foreach (scandir($parked_path) as $site) : ?>
                             <?php if ((is_dir("$parked_path/$site") || is_link("$parked_path/$site")) && $site[0] != '.') : ?>
                             <li><a href="http://<?= "$site.$tld" ?>/" target="<?= "valet_$site" ?>"
-                                class="text-blue hover:text-blue-light no-underline hover:underline"><?= "$site.$tld" ?></a></li>
+                                class="text-blue-500 hover:text-blue-400 no-underline hover:underline"><?= "$site.$tld" ?></a></li>
                             <?php endif ?>
                         <?php endforeach ?>
                     </ul>

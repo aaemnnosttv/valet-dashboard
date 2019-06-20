615b661b811f682b8af0acd0e4808c0a49a52816
marcriepen <klever.marc@gmail.com>
2019-06-20 14:03:10 +0200

    Included phpinfo() in dashboard. Dashboard itself is now excluded from sites list. List style is now numeric instead of using bullet points. Readme was adjusted to explain how to setup the dashboard as default valet site, so that it resolves under localhost

--- README.md
+++ README.md
@@ -2,13 +2,23 @@
 
 A simple dashboard for Laravel Valet to display all available sites.
 
-![image](https://user-images.githubusercontent.com/1621608/44817886-a515c180-abef-11e8-9126-9e0678283a9e.png)
+![image](preview.png)
 
 A super minimal dashboard which lists each available site for all parked paths as well as Valet-linked sites.
 
 ## Installation
 
-1. Clone or download the repo into a directory of your choosing  
-E.g. `git clone https://github.com/aaemnnosttv/valet-dashboard.git dashboard`
-1. Move the new directory into a Valet-parked path or run `valet link` within it
-1. Run `valet open` from within the directory!
+1. Clone or download the repo 
+E.g. `git clone https://github.com/aaemnnosttv/valet-dashboard.git`
+2. open `~/.config/valet/config.json` & add the `"default"` key:
+```json
+{
+    "domain": "dev",
+    "paths": [
+        "/Users/yourmac/yourpath"
+    ],
+    "default": "/Users/yourmac/yourpath/valet-dashboard",
+    "tld": "test"
+}
+```
+3. Go to http://localhost anytime to bring up your dashboard
--- index.php
+++ index.php
@@ -4,33 +4,41 @@ $valet_old_home = getenv('HOME') . '/.valet';
 $valet_home_path = is_dir($valet_xdg_home) ? $valet_xdg_home : $valet_old_home;
 $valet_config = json_decode(file_get_contents("$valet_home_path/config.json"));
 $tld = isset($valet_config->tld) ? $valet_config->tld : $valet_config->domain;
+$self = isset($valet_config->default) ? basename($valet_config->default) : 'valet-dashboard';
+
 ?>
 <html>
-    <head>
-        <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
-        <style>
-            .grid {
-                display: grid;
-                grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
-                justify-items: center;
-            }
-        </style>
-    </head>
-    <body class="m-12 font-sans">
-        <div class="grid">
+<head>
+    <title>Valet Dashboard</title>
+    <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
+</head>
+
+<body class="m-12 font-sans">
+
+    <div class="flex flex-wrap">
+        <div class="w-1/5">
             <?php foreach ($valet_config->paths as $parked_path) : ?>
-                <div class="leading-normal whitespace-no-wrap">
-                    <code class="font-mono text-gray-600"><?= str_replace(getenv('HOME'), '~', $parked_path) ?></code>
-                    <ul class="list-disc pl-4">
+                <div class="leading-normal whitespace-no-wrap font-mono text-gray-600">
+                    <code><?= str_replace(getenv('HOME'), '~', $parked_path) ?></code>
+                    <ol class="list-decimal pl-4 mt-4">
                         <?php foreach (scandir($parked_path) as $site) : ?>
                             <?php if ((is_dir("$parked_path/$site") || is_link("$parked_path/$site")) && $site[0] != '.') : ?>
-                            <li><a href="http://<?= "$site.$tld" ?>/" target="<?= "valet_$site" ?>"
-                                class="text-blue-500 hover:text-blue-400 no-underline hover:underline"><?= "$site.$tld" ?></a></li>
+                                <?php if ($site == $self): continue; endif ?>
+                                <li><a href="http://<?= "$site.$tld" ?>/" target="<?= "valet_$site" ?>"
+                                       class="text-blue-500 hover:text-blue-400 no-underline hover:underline"><?= "$site.$tld" ?></a>
+                                </li>
                             <?php endif ?>
                         <?php endforeach ?>
-                    </ul>
+                    </ol>
                 </div>
             <?php endforeach ?>
         </div>
-    </body>
+
+        <div class="w-4/5">
+            <iframe src="/info.php" frameborder="0" width="100%" height="100%"></iframe>
+        </div>
+
+    </div>
+
+</body>
 </html>
